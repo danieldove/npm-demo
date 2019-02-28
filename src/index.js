335365045c3bd5146206953298f284a6c78e3560
wangruizhe <859506185@qq.com>
2019-02-28 09:40:18 +0800

    use tree shaking

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import { cube } from './math.js';
 import printMe from './print.js';
 import './style.css';
 import Icon from './icon.svg';
 import Data from './data.xml';
 
 function component(){
-    var element = document.createElement('div');
+    // var element = document.createElement('div');
+    var element = document.createElement('pre');
     var btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack', '你好，中国！'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack', '你好，中国！'], ' ');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     element.classList.add('hello');
 
     btn.innerHTML = 'Click me and check the console!';
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
       path:path.resolve(__dirname, 'dist'),
       publicPath: '/'
   },
+  mode: "production",
   module:{
     rules: [
         {
